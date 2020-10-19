40b9f990f42c532845d58e23d2a5199c0d6d7987
Kamil Niemczycki <kamilniemczycki@gmail.com>
2020-10-19 17:31:48 +0200

    added: sass, bootstrap, jquery

--- public/js/app.js
+++ public/js/app.js
@@ -19323,17 +19323,6 @@ module.exports = function(module) {
 
 /***/ }),
 
-/***/ "./resources/css/app.css":
-/*!*******************************!*\
-  !*** ./resources/css/app.css ***!
-  \*******************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -19377,15 +19366,26 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!***********************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/css/app.css ***!
-  \***********************************************************/
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\kamil.DESKTOP-MOBILE\Desktop\rate-it\rate-it\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\kamil.DESKTOP-MOBILE\Desktop\rate-it\rate-it\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\kamil.DESKTOP-MOBILE\Desktop\rate-it\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\kamil.DESKTOP-MOBILE\Desktop\rate-it\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
