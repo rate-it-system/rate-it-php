c689acdd3ce7abc4081ef5c197818f4c6803a658
Kamil Niemczycki <kamilniemczycki@gmail.com>
2020-12-13 18:15:43 +0100

    rename class name

--- database/migrations/2020_12_13_142748_create_degustationfeatures_table.php
+++ database/migrations/2020_12_13_142748_create_degustationfeatures_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDegustationfeatures extends Migration
+class CreateDegustationfeaturesTable extends Migration
 {
     /**
      * Run the migrations.
