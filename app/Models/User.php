d0cc43bb0e397e19c1d6bb1268fc7def2f461672
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-01-16 14:15:01 +0100

    added api_token to $fillable & $hidden

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'login',
         'email',
         'password',
+        'api_token',
     ];
 
     /**
@@ -30,6 +31,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'api_token',
     ];
 
     /**
