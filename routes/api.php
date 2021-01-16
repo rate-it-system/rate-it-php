7bd964b637810e7519efae1873d0cde02322a488
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-01-16 14:22:22 +0100

    updated Api Router -> clean code, added login, added register

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,32 +16,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->name('api.')->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::name('degustations.')->group(function () {
+        Route::get('degustations', 'Api\DegustationController@index')
+            ->name('index');
+        Route::post('degustations', 'Api\DegustationController@store')
+            ->name('store');
+        Route::get('degustations/{degustation}', 'Api\DegustationController@show')
+            ->name('show');
+        Route::put('degustations/{degustation}', 'Api\DegustationController@update')
+            ->name('update');
+        Route::delete('degustations/{degustation}', 'Api\DegustationController@destroy')
+            ->name('destroy');
+    });
+
+    Route::name('products.')->group(function() {
+        Route::get('degustations/{degustation}/products', 'Api\ProductController@index')
+            ->name('index');
+        Route::post('degustations/{degustation}/products', 'Api\ProductController@store')
+            ->name('store');
+        Route::get('degustations/{degustation}/products/{product}', 'Api\ProductController@show')
+            ->name('show');
+        Route::put('degustations/{degustation}/products/{product}', 'Api\ProductController@update')
+            ->name('update');
+        Route::delete('degustations/{degustation}/products/{product}', 'Api\ProductController@destroy')
+            ->name('destroy');
+    });
 });
 
-Route::name('api.degustations.')->group(function () {
-    Route::get('degustations', 'Api\DegustationController@index')
-        ->name('index');
-    Route::post('degustations', 'Api\DegustationController@store')
-        ->name('store');
-    Route::get('degustations/{degustation}', 'Api\DegustationController@show')
-        ->name('show');
-    Route::put('degustations/{degustation}', 'Api\DegustationController@update')
-        ->name('update');
-    Route::delete('degustations/{degustation}', 'Api\DegustationController@destroy')
-        ->name('destroy');
-});
+Route::post('/login', [LoginController::class, 'loginNormal'])
+    ->name('api.login');
+Route::get('/login/{socialType}', [LoginController::class, 'redirectToProvider'])
+    ->name('api.login.socialmedia');
+Route::get('/login/{socialType}/callback', [LoginController::class, 'handleProviderCallback'])
+    ->name('api.login.socialmedia.callback');
 
-Route::name('api.products.')->group(function() {
-    Route::get('degustations/{degustation}/products', 'Api\ProductController@index')
-        ->name('index');
-    Route::post('degustations/{degustation}/products', 'Api\ProductController@store')
-        ->name('store');
-    Route::get('degustations/{degustation}/products/{product}', 'Api\ProductController@show')
-        ->name('show');
-    Route::put('degustations/{degustation}/products/{product}', 'Api\ProductController@update')
-        ->name('update');
-    Route::delete('degustations/{degustation}/products/{product}', 'Api\ProductController@destroy')
-        ->name('destroy');
-});
+Route::post('/register', [RegisterController::class, 'register'])
+    ->name('api.register');
